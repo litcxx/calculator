c58fa5e72e06c5131d6bff64ecbf0b461299e1ed
Grigorev Alexey <litk1pl@gmail.com>
2026-06-02 15:44:08 +0300

    feat: add getTask() method to Application

--- include/application.hpp
+++ include/application.hpp
@@ -11,11 +11,14 @@ class Application
   public:
     void run(int argc, char** argv);
 
+    const Task& getTask() const noexcept
+    {
+        return task_;
+    }
+
   private:
     void getTask(int argc, char** argv);
-
     void makeCalculate();
-
     void printResult() const;
 
   private:
